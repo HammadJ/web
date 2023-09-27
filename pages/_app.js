2a97d70069ce3733a4c4c1983e58ef9b01826a93
HammadJ <hammadjavaid2@gmail.com>
2023-09-27 16:29:24 +0500

    check

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
-// import '../public/assets/vendor/bootstrap/css/bootstrap.min.css'
 import '../styles/styles.pure.css'
 import '../public/assets/vendor/bootstrap-icons/bootstrap-icons.css'
 import '../public/assets/vendor/aos/aos.css'
-import '../public/assets/css/main.css'
 import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
