f3b3cae53076ac63e5af9bb9d979cdc20b0319c6
HammadJ <hammadjavaid2@gmail.com>
2023-09-27 13:47:15 +0500

    remove extra bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../public/assets/vendor/bootstrap/css/bootstrap.min.css'
+// import '../public/assets/vendor/bootstrap/css/bootstrap.min.css'
+import '../styles/styles.pure.css'
 import '../public/assets/vendor/bootstrap-icons/bootstrap-icons.css'
 import '../public/assets/vendor/aos/aos.css'
 import '../public/assets/css/main.css'
